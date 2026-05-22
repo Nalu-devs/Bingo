d396958fb4ad7e2256c39ab7060f3ebf88ac9ee5
Nalu-devs <pordosol699@gmail.com>
2026-05-22 08:26:19 -0300

    teste

--- src/main.js
+++ src/main.js
@@ -32,7 +32,6 @@ const router = new Router([
 
 document.getElementById('menuBtn').addEventListener('click', () => {
   const sidebar = document.getElementById('sidebar');
-  
   const overlay = document.getElementById('sidebar-overlay');
   sidebar.classList.toggle('hidden');
   overlay.classList.toggle('hidden');
