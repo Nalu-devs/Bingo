81fb3e0e03085b4a727c779ccb3215db8a244f9d
Nalu-devs <pordosol699@gmail.com>
2026-06-19 09:27:57 -0300

    teste

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-console.log('[main.js] Arcade Hub v1.1.0');
+console.log('[main.js] Arcade Hub v1.1.0 - Iniciando aplicacao');
 import './styles/main.css';
 import { Router, ScoreManager } from './core/index.js';
 import { HomePage } from './games/home/HomePage.js';
