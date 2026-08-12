2cb3bdc0e6fbc0bb44d4a58b803117a4a3ff7ccd
Nalu-devs <pordosol699@gmail.com>
2026-08-12 09:45:28 -0300

    teste console

--- src/game/GameState.js
+++ src/game/GameState.js
@@ -18,6 +18,7 @@ export class GameState {
 
   reset() {
     console.log("Essa mensagem é apenas para teste de log no construtor do GameState.js");
+    console.log("esse ela tem que pegar")
     console.log('[GameState.js] reset()');
     this.board = new Array(BOARD_SIZE).fill('');
     this.currentPlayer = SYMBOLS[0];
