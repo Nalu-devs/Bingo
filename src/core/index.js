7ae8b1edb88c12825d2f14369f15f7891565a533
Nalu-devs <pordosol699@gmail.com>
2026-08-18 10:34:35 -0300

    remove all files

--- src/core/index.js
+++
@@ -1,3 +0,0 @@
-console.log('[core/index.js] Carregado');
-export { Router } from './Router.js';
-export { ScoreManager } from './ScoreManager.js';
--- vitest.config.js
+++
@@ -1,9 +0,0 @@
-console.log('[vitest.config.js] Carregado');
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    include: ['src/**/*.test.js'],
-    environment: 'happy-dom',
-  },
-});
